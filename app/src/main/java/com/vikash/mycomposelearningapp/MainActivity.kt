fbc626e67dc2d621696410961ef059a2497de1ba
archive android <uttam.archive@gmail.com>
2023-12-25 10:42:37 +0530

    Added Image View with Custom Configs.

--- app/src/main/java/com/vikash/mycomposelearningapp/MainActivity.kt
+++ app/src/main/java/com/vikash/mycomposelearningapp/MainActivity.kt
@@ -6,14 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -58,10 +63,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             fontSize = 20.sp,
             textAlign = TextAlign.Center
         )
+        Spacer(modifier = Modifier.width(5.dp))
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_background),
             contentDescription = "Launcher",
-            Modifier.padding(10.dp)
+            Modifier
+                .size(48.dp)
+                .clip(CircleShape)
+                .padding(8.dp)
         )
     }
 }
