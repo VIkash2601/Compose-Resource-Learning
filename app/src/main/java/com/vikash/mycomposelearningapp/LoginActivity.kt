b1bc6e4f22c75b0a6df7708d26ab788c7cd62ae3
archive android <uttam.archive@gmail.com>
2024-01-30 12:04:56 +0530

    Fixed bug of typed text not reflecting. Added Logic for Password Toggle.

--- app/src/main/java/com/vikash/mycomposelearningapp/LoginActivity.kt
+++ app/src/main/java/com/vikash/mycomposelearningapp/LoginActivity.kt
@@ -5,6 +5,7 @@ package com.vikash.mycomposelearningapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Spacer
@@ -17,15 +18,25 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.vikash.mycomposelearningapp.ui.theme.MyComposeLearningAppTheme
@@ -52,19 +63,29 @@ class LoginActivity : ComponentActivity() {
 @Composable
 fun Login() {
 
-    val signInViewModel = SignInViewModel()
+    val signInViewModel = remember { SignInViewModel() }
+    var passwordVisibility: Boolean by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier
             .width(IntrinsicSize.Min)
             .padding(all = 5.dp)
-            .fillMaxHeight()
+            .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         OutlinedTextField(
             value = signInViewModel.email,
             onValueChange = { signInViewModel.updateEmail(it) },
             label = { Text("Enter Email") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min)
+                .padding(horizontal = 30.dp, vertical = 0.dp),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            )
         )
 
         Spacer(modifier = Modifier.height(10.dp))
@@ -73,17 +94,40 @@ fun Login() {
             value = signInViewModel.password,
             onValueChange = { signInViewModel.updatePassword(it) },
             label = { Text("Enter password") },
-            visualTransformation = PasswordVisualTransformation(),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+            visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min)
+                .padding(horizontal = 30.dp, vertical = 0.dp),
+            trailingIcon = {
+                IconButton(onClick = {
+                    passwordVisibility = !passwordVisibility
+                }) {
+                    Icon(
+                        painter = painterResource(
+                            if (passwordVisibility) R.drawable.ic_visibility_24
+                            else R.drawable.ic_visibility_off_24
+                        ),
+                        contentDescription = "toggle_password"
+                    )
+                }
+            }
         )
 
-        Spacer(modifier = Modifier.height(10.dp))
+        Spacer(modifier = Modifier.height(30.dp))
 
         Button(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(IntrinsicSize.Min),
-            onClick = {  }
+                .height(IntrinsicSize.Min)
+                .padding(horizontal = 30.dp, vertical = 0.dp),
+            onClick = {
+                signInViewModel.signIn()
+            }
         ) {
             Text(
                 text = "Login"
--- app/src/main/java/com/vikash/mycomposelearningapp/viewmodel/SignInViewModel.kt
+++ app/src/main/java/com/vikash/mycomposelearningapp/viewmodel/SignInViewModel.kt
@@ -1,20 +1,31 @@
 package com.vikash.mycomposelearningapp.viewmodel
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
-class SignInViewModel() : ViewModel() {
+class SignInViewModel : ViewModel() {
+
+    private val TAG = "SignInViewModel"
+
     var email by mutableStateOf("")
         private set
     var password by mutableStateOf("")
         private set
 
     fun updateEmail(input: String) {
+        Log.e(TAG, "Updating email: $input")
         email = input
     }
+
     fun updatePassword(input: String) {
+        Log.e(TAG, "Updating password: $input")
         password = input
+    }
+
+    fun signIn() {
+        Log.e(TAG, "signIn: $email & $password")
     }
 }
